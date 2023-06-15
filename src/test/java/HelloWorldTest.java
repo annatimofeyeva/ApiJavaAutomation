72a6170626807ec1fb183f52302331994c437bca
Anna Timofeeva <anjatimofeyeva@gmail.com>
2023-06-14 20:51:24 -0700

    firstApiTest() added

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,18 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
         System.out.println("Hello world");
     }
+    @Test
+    public void firstApiTest() {
+        Response responce = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        responce.prettyPrint();
+    }
 }
+
