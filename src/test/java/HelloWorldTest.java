b7763f730d8fe29acd47ff3e9051bf138524078b
Anna Timofeeva <anjatimofeyeva@gmail.com>
2023-06-15 11:04:47 -0700

    Greetings from me code added

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -5,7 +5,7 @@ import io.restassured.response.Response;
 public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
-        System.out.println("Hello world");
+        System.out.println("Hello from Anna");
     }
     @Test
     public void firstApiTest() {
